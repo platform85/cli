8b73859a42f7ff6e857e12486e46f6954a885535
Sergei O. Udalov <udalov.x@mail.ru>
2023-11-19 09:10:48 +0300

    gemspec fixed

--- spec/platform85/cli_spec.rb
+++ spec/platform85/cli_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Platform85::Cli do
   it "has a version number" do
     expect(Platform85::Cli::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
