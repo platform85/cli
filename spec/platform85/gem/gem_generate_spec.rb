c03be2f7353030137f43f13c21723cb97da7fc2e
Sergei O. Udalov <udalov.x@mail.ru>
2023-11-19 11:37:23 +0300

    new gem command

--- spec/platform85/gem/gem_generate_spec.rb
+++ spec/platform85/gem/gem_generate_spec.rb
@@ -6,10 +6,8 @@ module Platform85
   module Gem
     RSpec.describe NewGem do
       example "it can generate new gem " do
-
-        Dev::Test::TempDirectory.new.with do |dir|
-          puts dir.inspect
-          NewGem.new("new-library", dir:).generate
+        Dev::Test::TempDirectory.new.with do
+          NewGem.new("new-library", {}).generate
         end
       end
     end
