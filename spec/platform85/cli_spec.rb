7a909cccae13b8cbbd5b53dcea8d8ce57d06f3fb
Sergei O. Udalov <udalov.x@mail.ru>
2023-11-19 09:07:31 +0300

    init

---
+++ spec/platform85/cli_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Platform85::Cli do
+  it "has a version number" do
+    expect(Platform85::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
