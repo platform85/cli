78dd847cc1969be6d30ab836e2ae2efc704cdf41
Sergei O. Udalov <udalov.x@mail.ru>
2023-11-22 07:55:09 +0300

    patch_01 07:55

--- lib/platform85/gem/new_gem.rb
+++ lib/platform85/gem/new_gem.rb
@@ -5,5 +5,8 @@ require "thor/actions"
 
 module Platform85
   module Gem
+    class NewGem
+
+    end
   end
 end
--- spec/platform85/gem/gem_generate_spec.rb
+++ spec/platform85/gem/gem_generate_spec.rb
@@ -9,9 +9,15 @@ module Platform85
         `#{RootPath.new.join("bin/platform85")} #{cmd}`
       end
 
+      def expect_dir_to_be_present(name)
+        expect(Dir.exist?(name)).to be_truthy
+      end
+
       example "it can generate new gem " do
         Dev::Test::TempDirectory.new.with do
           platform_exec("gem new new-gem")
+
+          expect_dir_to_be_present("new-gem")
         end
       end
     end
